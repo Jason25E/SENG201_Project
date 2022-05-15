39785baaf55d0545c3669ad1e53fee2a2ab17508
Rain <zde19@uclive.ac.nz>
2022-05-16 11:19:14 +1200

    UPDATE

--- SENG201_Project/src/SetupScreen.java
+++ SENG201_Project/src/SetupScreen.java
@@ -1,10 +1,18 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JSlider;
+import java.awt.Color;
 
 public class SetupScreen {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -26,6 +34,7 @@ public class SetupScreen {
 	 * Create the application.
 	 */
 	public SetupScreen() {
+		
 		initialize();
 	}
 
@@ -36,6 +45,50 @@ public class SetupScreen {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblWhatIsYour = new JLabel("What is your name");
+		lblWhatIsYour.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblWhatIsYour.setBounds(125, 7, 200, 20);
+		frame.getContentPane().add(lblWhatIsYour);
+		
+		textField = new JTextField();
+		textField.setBounds(115, 39, 200, 25);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("How many days would you like to last for?");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblNewLabel.setBounds(24, 76, 400, 20);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblChooseADifficulty = new JLabel("Choose a difficulty");
+		lblChooseADifficulty.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblChooseADifficulty.setBounds(125, 156, 200, 20);
+		frame.getContentPane().add(lblChooseADifficulty);
+		
+		JButton btnEasy = new JButton("Easy");
+		btnEasy.setBounds(24, 198, 117, 25);
+		frame.getContentPane().add(btnEasy);
+		
+		JButton btnNormal = new JButton("Normal");
+		btnNormal.setBounds(169, 198, 117, 25);
+		frame.getContentPane().add(btnNormal);
+		
+		JButton btnHard = new JButton("Hard");
+		btnHard.setBounds(307, 198, 117, 25);
+		frame.getContentPane().add(btnHard);
+		
+		JSlider slider = new JSlider();
+		slider.setMinimum(5);
+		slider.setMaximum(15);
+		slider.setBounds(115, 120, 200, 25);
+		frame.getContentPane().add(slider);
+		
+		JButton btnNewButton = new JButton("Next");
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(0, 204, 102));
+		btnNewButton.setBounds(319, 235, 117, 25);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
